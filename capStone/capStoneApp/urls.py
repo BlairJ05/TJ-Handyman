f38feb88c4b9eaf4e04d17a50afc3dce99242226
ConnerCleland <Chcleland2@gmail.com>
2024-03-20 13:33:20 -0500

    added templates for the signUp and signIn

--- capStone/capStoneApp/urls.py
+++ capStone/capStoneApp/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import index
+from .views import index, signUp,signIn
 
 urlpatterns = [
     path('', index, name='index'),
+    path('signUp/',signUp, name='signUp'),
+    path('signIn/',signIn, name='signIn'),
 ]
