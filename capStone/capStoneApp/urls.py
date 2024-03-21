083fda6bdce3be859bc6fc2a0f821f9b271071aa
ConnerCleland <Chcleland2@gmail.com>
2024-03-21 14:13:25 -0500

    added login features

--- capStone/capStoneApp/urls.py
+++ capStone/capStoneApp/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
-from .views import index, signUp,signIn
+from .views import index, signUp,signIn,signUp,signOut
 
 urlpatterns = [
     path('', index, name='index'),
     path('signUp/',signUp, name='signUp'),
     path('signIn/',signIn, name='signIn'),
+    path('signUp/',signUp, name='signUp'),
+    path('signOut/',signOut, name='signOut'),
+
+
 ]
--- capStone/capStoneApp/views.py
+++ capStone/capStoneApp/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
+from django.shortcuts import redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate, login
+from django.contrib.auth import logout
+from django.contrib.auth.forms import AuthenticationForm
+
 
 def index(request):
     return render(request, 'index.html')
@@ -8,4 +14,36 @@ def signUp(request):
     return render(request, 'signUp.html')
 
 def signIn(request):
-    return render(request, 'signIn.html')
+    if request.method == 'POST':
+        form = AuthenticationForm(request, request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('index')  # Redirect to the index page after successful login
+    else:
+        form = AuthenticationForm()
+    return render(request, 'signIn.html', {'form': form})
+
+def signUp(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('signIn')
+    else:
+        form = UserCreationForm()
+    return render(request, 'signUp.html', {'form': form})
+
+
+def signOut(request):
+    logout(request)
+    return redirect('index')
+
