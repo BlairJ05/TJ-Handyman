f033e82f2cc9e811cd2737a306b8d591caa52db4
ConnerCleland <Chcleland2@gmail.com>
2024-03-25 15:03:04 -0500

    added more onto the pages and fixed cards

--- capStone/capStoneApp/urls.py
+++ capStone/capStoneApp/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import index, signUp,signIn,signUp,signOut
+from .views import index, signUp,signIn,signUp,signOut,Assembly,Carpenter,Home_Repairs,Instulation,More_About_Me,Moving,Outdoor_Help,Painting
 
 urlpatterns = [
     path('', index, name='index'),
@@ -7,6 +7,16 @@ urlpatterns = [
     path('signIn/',signIn, name='signIn'),
     path('signUp/',signUp, name='signUp'),
     path('signOut/',signOut, name='signOut'),
+    path('Assembly/',Assembly, name='Assembly'),
+    path('Carpenter/',Carpenter, name='Carpenter'),
+    path('Home_Repairs/',Home_Repairs, name='Home_Repairs'),
+    path('Instulation/',Instulation, name='Instulation'),
+    path('More_About_Me/',More_About_Me, name='More_About_Me'),
+    path('Moving/',Moving, name='Moving'),
+    path('Painting/',Painting, name='Painting'),
+    path('Outdoor_Help/',Outdoor_Help, name='Outdoor_Help'),
+
+
 
 
 ]
