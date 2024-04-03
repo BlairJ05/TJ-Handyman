d90d70aa9a52ff4e7a5878be9ebd85dcb85c1514
ConnerCleland <Chcleland2@gmail.com>
2024-04-03 14:38:13 -0500

    added modles

--- capStone/capStoneApp/models.py
+++ capStone/capStoneApp/models.py
@@ -1,1 +1,8 @@
 from django.db import models
+from datetime import datetime
+
+class Message(models.Model):
+    value = models.CharField(max_length=100)
+    date = models.DateTimeField(default=datetime.now, blank=True)
+    user = models.CharField(max_length=100)
+    room = models.CharField(max_length=100)
