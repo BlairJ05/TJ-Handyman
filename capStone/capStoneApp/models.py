6e21fd57af2fe39527c777ab6ae05d5a31f0786a
ConnerCleland <Chcleland2@gmail.com>
2024-04-04 09:02:13 -0500

    trying to make the chatbot

--- capStone/capStoneApp/models.py
+++ capStone/capStoneApp/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 from datetime import datetime
 
+class Room(models.Model):
+    name = models.CharField(max_length=100)
+    
 class Message(models.Model):
     value = models.CharField(max_length=100)
     date = models.DateTimeField(default=datetime.now, blank=True)
