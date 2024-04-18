e0a2032651664bdb161f4a774598f83544e56dfd
Blair <blairjohnson562@gmail.com>
2024-04-18 15:45:49 -0500

    fixed front end and back end on Gallary, fixed nav bar view depending on whos logged in, started reviews section

--- capStone/capStoneApp/models.py
+++ capStone/capStoneApp/models.py
@@ -10,9 +10,10 @@ class Review(models.Model):
     def __str__(self):
         return f"Review by {self.user.username}"
 class CreateCard(models.Model):
+    header = models.CharField(max_length=30, null=True)
     before_Pic = models.ImageField(upload_to="capStoneApp/static/project_images")
     after_Pic = models.ImageField(upload_to="capStoneApp/static/project_images")
     title = models.CharField(max_length=200, null=True)
     description = models.CharField(max_length=2000, null=True)
-    facebook_url = models.URLField(max_length=200, null=True, blank=True)
+    url = models.URLField(max_length=200, null=True, blank=True)
     
