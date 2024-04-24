dcbfce2e04fccf9d95ed23896571b9b602ef2d1c
Jordan <jdonahue24@basecampcodingacademt.org>
2024-04-24 16:00:11 -0500

    Requirements.txt

--- capStone/settings.py
+++ capStone/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -122,3 +124,8 @@ STATIC_URL = "/static/"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+# Disable collectstatic during deployment
+if os.getenv("DISABLE_COLLECTSTATIC"):
+    STATICFILES_STORAGE = "django.contrib.staticfiles.storage.StaticFilesStorage"
