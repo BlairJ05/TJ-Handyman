fbb63bc77722e8d9e273a61902f2ee39e122de9a
Jordan <jdonahue24@basecampcodingacademt.org>
2024-04-26 08:35:47 -0500

    Collect static

--- capStoneApp/admin.py
+++ capStoneApp/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from .models import Review, CreateCard, Chat
+from .models import Review, CreateCard
 
 admin.site.register(Review)
 admin.site.register(CreateCard)
-admin.site.register(Chat)
+# admin.site.register(Chat)
