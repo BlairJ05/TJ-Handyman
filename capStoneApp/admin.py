315e44c539440e15bec0936e94605059b32fb9af
Blair <blairjohnson562@gmail.com>
2024-04-26 11:42:39 -0500

    Blair is a stinky head who didnt know whether to push or pull so we are going to do both :)

--- capStoneApp/admin.py
+++ capStoneApp/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Review, CreateCard
+from .models import Review, CreateCard, Chat
 
 admin.site.register(Review)
 admin.site.register(CreateCard)
